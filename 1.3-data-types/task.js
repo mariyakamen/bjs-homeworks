c20e945065ef76caeea7a070500c76f9ef52b83a
Mariya Kamen <mari.mts144@gmail.com>
2020-09-13 14:22:26 +0200

    3rd homework ready

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,50 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (typeof (percent) !== typeof (1) || isNaN(percent)) {
+        let message = "ПРОЦЕНТ ДОЛЖЕН БЫТЬ ЧИСЛОМ!";
+        console.log(message);
+        return message;
+    } else if (typeof (contribution) !== typeof (1) || isNaN(contribution)) {
+        let message = "НАЧАЛЬНЫЙ ВЗНОС ДОЛЖЕН БЫТЬ ЧИСЛОМ!";
+        console.log(message);
+        return message;
+    } else if (typeof (amount) !== typeof (1) || isNaN(amount)) {
+        let message = "ОБЩАЯ СТОИМОСТЬ ДОЛЖНА БЫТЬ ЧИСЛОМ!";
+        console.log(message);
+        return message;
+    } else if ((typeof (date) !== typeof (new Date())) || date.toString() === "Invalid Date") {
+        let message = "УКАЖИТЕ ДАТУ";
+        console.log(message);
+        return message;
+    }
+
+    let returnAmount = amount - contribution;
+    let nowYear = new Date().getFullYear();
+    let nowMonth = new Date().getMonth();
+    let n = (date.getFullYear() - nowYear) * 12 + (date.getMonth() - nowMonth);
+    let P = percent / 100 / 12;
+    let totalAmount = n * (returnAmount * (P + P / (((1 + P) ** n) - 1)));
+
+    totalAmount = totalAmount.toFixed(2);
+    console.log(totalAmount);
+
+    return Number(totalAmount);
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (name)
+        greeting = (`Привет, мир! Меня зовут ${name}`);
+
+    else
+        greeting = (`Привет, мир! Меня зовут Аноним`);
+
+    console.log(greeting);
+
+
+    return greeting;
 }
